423a839993151a996ba6b8f825bebbca0bb76924
Misha Causur <mishacausur@mail.ru>
2024-10-06 22:09:56 +0500

    five steps

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	array = FiveSteps(array)
+	fmt.Printf("%v", array)
 }
 
-func MaxInt(a, b int) int {
-	if a >= b {
-		return a
-	}
+func FiveSteps(array [5]int) [5]int {
+	var reversed [5]int
 
-	return b
+	for i := 0; i < len(array); i++ {
+		reversed[i] = array[len(array)-1-i]
+	}
+	return reversed
 }
