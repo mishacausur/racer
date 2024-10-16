dcf5b8ed848f9000f9016dacb86ba792735db169
Misha Causur <mishacausur@mail.ru>
2024-10-16 21:47:46 +0500

    animal

--- main.go
+++ main.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type Shape interface {
-	Area() float64
+type Animal interface {
+	MakeSound()
 }
 
-type Circle struct {
-	radius float64
+type Cat struct{}
+
+func (c Cat) MakeSound() {
+	fmt.Println("Мяу!")
 }
 
-func (c Circle) Area() float64 {
-	return math.Pi * (c.radius * c.radius)
-}
+type Dog struct{}
 
-type Rectangle struct {
-	width  float32
-	height float32
-}
-
-func (r Rectangle) Area() float64 {
-	return float64(r.height * r.width)
-}
-
-func main() {
-	figure_1 := Circle{radius: 1.0}
-	fmt.Println(figure_1.Area())
-	figure_2 := Rectangle{width: 57.2, height: 10.2}
-	fmt.Println(figure_2.Area())
+func (c Dog) MakeSound() {
+	fmt.Println("Гав!")
 }
