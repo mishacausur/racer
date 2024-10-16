a7bce6fc01074b648104c27aa391b08f79419f16
Misha Causur <mishacausur@mail.ru>
2024-10-16 21:24:40 +0500

    todo

--- main.go
+++ main.go
@@ -1,16 +1,68 @@
 package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+	"time"
+)
 
-type Student struct {
-	name            string
-	solvedProblems  int
-	scoreForOneTask float64
-	passingScore    float64
+type Task struct {
+	summary     string
+	description string
+	deadline    time.Time
+	priority    int
 }
 
-func (s Student) IsExcellentStudent() bool {
-	return s.passingScore < (float64(s.solvedProblems) * s.scoreForOneTask)
+type Note struct {
+	title string
+	text  string
+}
+
+type ToDoList struct {
+	name  string
+	tasks []Task
+	notes []Note
+}
+
+func (t Task) IsOverdue() bool {
+	return time.Now().After(t.deadline)
+}
+
+func (t Task) IsTopPriority() bool {
+	return t.priority >= 4
+}
+
+func (l ToDoList) TasksCount() int {
+	return len(l.tasks)
+}
+
+func (l ToDoList) NotesCount() int {
+	return len(l.notes)
+}
+
+func (l ToDoList) CountTopPrioritiesTasks() int {
+	var result int
+	for _, i := range l.tasks {
+		if i.priority >= 4 {
+			result++
+		}
+	}
+
+	return result
+}
+
+func (l ToDoList) CountOverdueTasks() int {
+	var result int
+	for _, i := range l.tasks {
+		if i.IsOverdue() {
+			result++
+		}
+	}
+
+	return result
+}
+
+func main() {
+	task := Task{summary: "Make Yandex Lyceum", deadline: time.Now().Add(time.Hour), description: "Make Module 0, Task 10", priority: 4}
+	fmt.Println(task.IsOverdue())
+	fmt.Print(task.IsTopPriority())
 }
