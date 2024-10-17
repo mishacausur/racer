699d7159e6cf90da98773dc105d6261147690c9d
Misha Causur <mishacausur@mail.ru>
2024-10-17 21:24:22 +0500

    recover from panic

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-var (
-	ErrDivisionByZero = errors.New("division by zero")
-)
-
-func divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, ErrDivisionByZero
-	}
-	return a / b, nil
-}
-
 func main() {
-	result, err := divide(10, 0)
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
-	}
-	fmt.Println("Результат:", result)
+	fmt.Println("Start")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
+
+	panic("Something went wrong")
+	fmt.Println("End")
 }
